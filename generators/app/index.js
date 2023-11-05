d04684ee04d4221beb956d91af755757b2665a83
Owen Trueblood <hi@owentrueblood.com>
2023-11-05 11:59:53 -0500

    Explicitly specify Yarn as the package manager

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,8 @@ const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
   writing() {
+    this.env.options.nodePackageManager = 'yarn';
+
     this.fs.copyTpl(
       this.templatePath('**/*'),
       this.destinationPath(),
